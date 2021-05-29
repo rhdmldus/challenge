ec172f0b370d7dfaff6f35aeae2d7eb029fa3ea1
rhdmldus <76199546+rhdmldus@users.noreply.github.com>
2021-05-29 18:21:24 +0900

    카피기능첨가

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
-// fech the items from the JSON file
+// data불러오기
 function loadItems() {
   return fetch("data/data.json")
     .then((response) => response.json())
     .then((json) => json.items);
 }
-
-function displayItems(items) {
+// 아이템 뿌리기
+function addItems(items) {
   const container = document.querySelector(".items");
-  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+  container.innerHTML = items.map((item) => createElement(item)).join("");
+  console.log(items.map((item) => createElement(item)).join(""));
+  console.log(items);
+}
+// 아이템생성하기
+function createElement(item) {
+  return `
+  <li class="item">${item.face}</li>
+  `;
+}
+// 클릭함수
+function onbtnClick(event, items) {
+  const dataset = event.target.dataset;
+  console.log(dataset);
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+  addItems(items.filter((item) => item[key] === value));
+}
+// 이벤트리스너
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const btns = document.querySelector(".btn_wrap");
+  const itemCopy = document.querySelector(".items");
+  logo.addEventListener("click", () => addItems(items));
+  btns.addEventListener("click", () => onbtnClick(event, items));
+  itemCopy.addEventListener("click", () => copyText(event, items));
 }
 
-function createHTMLString(item) {
-  return `
-    <li class="item">
-        <span class="thumb_nail">${item.image}</span>
-        <span class="item_description">${item.gender}, ${item.size}</span>
-    </li>
-    `;
+function copyText(event) {
+  const imoji = event.target.innerHTML;
+  const copyed = document.createElement("input");
+  document.body.appendChild(copyed);
+  copyed.value = imoji;
+  copyed.select();
+  document.execCommand("copy");
+  copyed.remove();
+  showMsg(event);
 }
-// main
-loadItems()
-  .then((items) => {
-    console.log(items);
-    displayItems(items);
-    // setEventListeners(items);
-  })
-  .catch(console.log);
+function showMsg(event) {
+  const target = event.target;
+  target.classList.add("copied");
+  setTimeout(() => {
+    target.classList.remove("copied");
+  }, 500);
+}
+// 최종
+loadItems().then((items) => {
+  addItems(items);
+  setEventListeners(items);
+});
